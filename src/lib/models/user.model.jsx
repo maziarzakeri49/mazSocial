525658d3f78bc246c2ec7c05f33167d67ebbfb29
maziarzakeri49 <maziarzakeri49@gmail.com>
2024-12-10 23:35:25 -0500

    make webhooks

---
+++ src/lib/models/user.model.jsx
@@ -0,0 +1,47 @@
+import mongoose from "mongoose";
+import { unique } from "next/dist/build/utils";
+
+const userSchema=mongoose.Schema(
+    {
+        clerkID:{
+            type:String,
+            required:true ,
+            unique:true
+        },
+        email:{
+            type:String,
+        },
+        firstName:{
+            type:String,
+            required:true ,
+        },
+        lastName:{
+            type:String,
+            required:true ,
+
+        },
+        username:{
+            type:String,
+            required:true ,
+
+        },
+        avetar:{
+            type:String,
+            required:true ,
+
+        },
+        followers:{
+            type:[{type:mongoose.Schema.Types.ObjectId , ref:'User'}],
+            default:[],
+        },
+        following:{
+            type:[{type:mongoose.Schema.Types.ObjectId , ref:'User'}],
+            default:[],
+
+        }
+
+        
+    },{timestamps:true}
+);
+const User=mongoose.model.User || mongoose.model('User',userSchema)
+export default User ;
--- src/lib/mongodb/mongoose.jsx
+++ src/lib/mongodb/mongoose.jsx
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 let initialized = false;
 
 export const connect = async ()=>{
-    mongoose.setI('strictQuery' , true)
+    mongoose.set('strictQuery' , true)
     if(initialized){
         console.log('Already connected to MongoDB');
         return;
